6f3f06526e60ead14a630e4c8d12a4cc2b6084fe
dwicao <lutfiandwica@gmail.com>
2017-03-14 09:17:43 +0700

    fix example deps

--- example/index.ios.js
+++ example/index.ios.js
@@ -5,13 +5,19 @@ import {
   Text,
   View,
 } from 'react-native';
-import Main from './src/components/Main';
+import CrossPlatformIcon from 'react-native-cross-platform-icons';
 
 export default class sparkClick extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Main />
+        <Text>react-native-cross-platform-icons</Text>
+        <CrossPlatformIcon name="home" size={30} color="#000000" />
+        <CrossPlatformIcon name="home" size={30} color="#000000" outline />
+        <CrossPlatformIcon name="basket" size={25} color="#000000" />
+        <CrossPlatformIcon name="jet" size={35} color="blue" />
+        <CrossPlatformIcon name="arrow-back" size={32} color="red" />
+        <CrossPlatformIcon name="send" size={40} color="#bada55" />
       </View>
     );
   }
@@ -20,6 +26,8 @@ export default class sparkClick extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
 
